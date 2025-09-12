ef714020d7cb2facd9635ccf09ee835699b43eb4
tywr <tanguy.viv@gmail.com>
2025-09-12 16:35:48 +0200

    initial commit

---
+++ makefile
@@ -0,0 +1,3 @@
+PYTHON = python3.11
+setup:
+	${PYTHON} setup.py sdist bdist_wheel
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nansen",
+    version="0.1.0",
+    author="Tanguy VIVIER",
+    packages=find_packages(),
+    install_requires=["numpy", "pandas", "matplotlib"],
+)
